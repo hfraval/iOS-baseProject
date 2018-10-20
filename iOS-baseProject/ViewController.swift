0a7eb4c0b67dd5a7f653894ed708f66608c45daa
Herve Fraval <hfraval.creativethinking@gmail.com>
2018-10-20 17:45:32 +1100

    xcode project creation

---
+++ iOS-baseProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-baseProject
+//
+//  Created by herve fraval on 20/10/18.
+//  Copyright © 2018 herve fraval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
